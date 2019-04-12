987a4e6539705f3363474ee54767ca6179cf7123
HP <HP@DV6-6170>
2019-04-11 22:39:56 -0300

    Inclusao do caso de teste para verificar erro 404

--- sce/src/test/java/com/fatec/sce/UC01ConsultaLivro.java
+++ sce/src/test/java/com/fatec/sce/UC01ConsultaLivro.java
@@ -14,8 +14,8 @@ import com.fatec.sce.model.Livro;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UC01ConsultaLivro extends AbstractTest {
 	@Test
-	public void getProductsList() throws Exception {
-	   String uri = "/livros";
+	public void CT01getProductsList() throws Exception {
+	   String uri = "/livro";
 	   setUp();
 	   MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
 	      .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
@@ -25,5 +25,22 @@ public class UC01ConsultaLivro extends AbstractTest {
 	   String content = mvcResult.getResponse().getContentAsString();
 	   Livro[] productlist = super.mapFromJson(content, Livro[].class);
 	   assertTrue(productlist.length > 0);
+	}
+	/**
+	 * Verifica o comportamento na chamada de um endereco invalido
+	 * O erro 404 é um código de resposta HTTP que indica que o cliente pôde comunicar com 
+	 * o servidor, mas ou o servidor não pôde encontrar o que foi pedido 
+	 * @throws Exception
+	 */
+	@Test
+	public void CT02getProductsList() throws Exception {
+	   String uri = "/livros";
+	   setUp();
+	   MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+	      .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+	   
+	   int status = mvcResult.getResponse().getStatus();
+	   assertEquals(404, status);
+	  
 	}
 }
